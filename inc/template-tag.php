b894f885aa14648c3f67c0ca0de3e919cad39bce
tienhtpd02242 <48255077+thangtienql@users.noreply.github.com>
2023-10-16 10:11:02 +0700

    UPDATE 16-10
    UPDATE 16-10

--- inc/template-tag.php
+++ inc/template-tag.php
@@ -8,6 +8,9 @@ function SAH_product_listing_tag() {
   $query = SAH_get_products($args);
   ?>
 <div class="sah-product-listing">
+
+    <div class="filter-mobile" id="filterProduct">Filter</div>
+
     <div class="sah-product-listing__items">
         <?php SAH_product_loop_tag($query) ?>
     </div> <!-- .sah-product-listing__items -->
@@ -21,125 +24,90 @@ function SAH_product_loop_tag($query) {
   while ( $query->have_posts() ) {
     $query->the_post();
     ?>
-<div <?php post_class(apply_filters('SAH/product-loop-item-classes-filter', 'sah-product-item')); ?>>
-    <div class="sah-product-item__box">
-        <div class="sah-product-item__left">
-            <div class="sah-product-item__feaftured-img">
-                <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title();?>">
-            </div>
-            <div class="sah-product-item__info">
-                <a href="<?php the_permalink();?>">
-                    <h3><?php the_title();?></h3>
-                </a>
-
-                <?php if ( !empty(get_field('company_name')) ) { ?>
-                <div class="company">by <?php echo get_field('company_name');?></div>
-                <?php } ?>
-
-                <?php if ( !empty(get_field('rating')) ) { ?>
-                <div class="rating">
-                    <?php echo sah_rating_render( get_field('rating') , get_field('total_rating') );?>
+    <div <?php post_class(apply_filters('SAH/product-loop-item-classes-filter', 'sah-product-item')); ?>>
+        <div class="sah-product-item__box">
+            <div class="sah-product-item__left">
+                <div class="sah-product-item__feaftured-img">
+                    <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title();?>">
                 </div>
-                <?php } ?>
+                <div class="sah-product-item__info">
+                    <a href="<?php the_permalink();?>">
+                        <h3><?php the_title();?></h3>
+                    </a>
+
+                    <?php if ( !empty(get_field('company_name')) ) { ?>
+                    <div class="company">by <?php echo get_field('company_name');?></div>
+                    <?php } ?>
+
+                    <?php if ( !empty(get_field('rating')) ) { ?>
+                    <div class="rating">
+                        <?php echo sah_rating_render( get_field('rating') , get_field('total_rating') );?>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+            <div class="sah-product-item__right">
+                <a href="<?php the_permalink();?>">View Profile</a>
             </div>
         </div>
-
-        <div class="sah-product-item__right">
-            <a href="<?php the_permalink();?>">View Profile</a>
+        <div class="sah-product-item__description">
+            <?php 
+            if( has_excerpt() ){
+              echo wp_trim_words(strip_shortcodes( get_the_excerpt()), 40 );
+            }
+            ?>
+            <a href="<?php the_permalink();?>">Learn more about <?php the_title();?></a>
         </div>
     </div>
-    <div class="sah-product-item__description">
-        <?php the_excerpt();?>
-        <a href="<?php the_permalink();?>">Learn more about <?php the_title();?></a>
-    </div>
-</div>
-<?php
+    <?php
   }
 }
 
 function SAH_sidebar_listing(){
 
   $taxonomies = get_taxonomies( [ 'object_type' => [ 'saas-adapt-product' ] ] );
-  print_r($taxonomies);
+  if ( !empty($taxonomies) ) {
+    foreach ($taxonomies as $key => $taxonomy) {
 
-  $terms_pricing_opts = get_terms( array(
-    'taxonomy' => 'product-pricing-options',
-    'hide_empty' => false,
-  ) );
+      $taxonomy_details = get_taxonomy( $taxonomy );
+      // print_r($taxonomy_details);
 
-  $terms_features = get_terms( array(
-    'taxonomy' => 'product-features',
-    'hide_empty' => false,
-  ) );
+      $terms = get_terms( array(
+        'taxonomy' => $taxonomy,
+        'hide_empty' => false,
+      ) );
 
-  $terms_deployment = get_terms( array(
-    'taxonomy' => 'product-deployment',
-    'hide_empty' => false,
-  ) );
+      if ( !empty($terms) ) {
+        echo "<div class='product-options'>";
+          echo "<h4>". $taxonomy_details->label ."</h4>";
+          foreach ($terms as $term) { ?>
+            <div class="box">
+                <input type="checkbox" id="<?php echo $term->slug;?>" name="pricing-options"
+                    value="<?php echo $term->slug;?>">
+                <label for="<?php echo $term->slug;?>"><?php echo $term->name;?></label><br>
+            </div>
+          <?php }
+        echo "</div>";
+      }
 
-  $terms_users_range = get_terms( array(
-    'taxonomy' => 'product-users-range',
-    'hide_empty' => false,
-  ) );
-
-  if ( !empty($terms_pricing_opts) ) {
-    echo "<div class='product-options'>";
-      echo "<h4>Pricing Options</h4>";
-      foreach ($terms_pricing_opts as $term_pricing_opt) { ?>
-        <div class="box">
-            <input type="checkbox" id="<?php echo $term_pricing_opt->slug;?>" name="pricing-options"
-                value="<?php echo $term_pricing_opt->slug;?>">
-            <label for="<?php echo $term_pricing_opt->slug;?>"><?php echo $term_pricing_opt->name;?></label><br>
-        </div>
-      <?php }
-    echo "</div>";
+    }
   }
 
-  if ( !empty($terms_features) ) {
-    echo "<div class='product-options'>";
-      echo "<h4>Features</h4>";
-      foreach ($terms_features as $term_feature) { ?>
-        <div class="box">
-            <input type="checkbox" id="<?php echo $term_feature->slug;?>" name="pricing-options"
-                value="<?php echo $term_feature->slug;?>">
-            <label for="<?php echo $term_feature->slug;?>"><?php echo $term_feature->name;?></label><br>
-        </div>
-      <?php }
-    echo "</div>";
-  }
-
-  if ( !empty($terms_deployment) ) {
-    echo "<div class='product-options'>";
-      echo "<h4>Deployment</h4>";
-      foreach ($terms_deployment as $term_deployment) { ?>
-        <div class="box">
-            <input type="checkbox" id="<?php echo $term_deployment->slug;?>" name="pricing-options"
-                value="<?php echo $term_deployment->slug;?>">
-            <label for="<?php echo $term_deployment->slug;?>"><?php echo $term_deployment->name;?></label><br>
-        </div>
-      <?php }
-    echo "</div>";
-  }
-
-  if ( !empty($terms_users_range) ) {
-    echo "<div class='product-options'>";
-      echo "<h4>Users</h4>";
-      foreach ($terms_users_range as $user_range) { ?>
-        <div class="box">
-            <input type="checkbox" id="<?php echo $user_range->slug;?>" name="pricing-options"
-                value="<?php echo $user_range->slug;?>">
-            <label for="<?php echo $user_range->slug;?>"><?php echo $user_range->name;?></label><br>
-        </div>
-      <?php }
-    echo "</div>";
-  }
-  
 }
 
 function SAH_paginations_tag($query) {
   ?>
-<div class="sah-paginations">
-
-</div> <!-- .sah-paginations -->
+  <div class="sah-paginations">
+  <?php
+  $big = 999999999; // need an unlikely integer
+  echo paginate_links( array(
+    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+    'format' => '?paged=%#%',
+    'current' => max( 1, get_query_var('paged') ),
+    'total' => $query->max_num_pages
+  ) );
+  ?>
+  </div> <!-- .sah-paginations -->
 <?php
 }
--- templates/product-listing-sidebar.php
+++ templates/product-listing-sidebar.php
@@ -5,6 +5,11 @@
 
 ?>
 <div class="sah-product-listing-page__sidebar">
+  <div class="close-btn">
+    <svg width="24" height="24" class="icon-close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.5 4.5L19.5005 19.5005M4.5 19.5005L19.5005 4.5" stroke="#333333" stroke-width="1.5" stroke-linecap="square"></path>
+    </svg>
+  </div>
   <div class="sah-product-listing__sidebar-inner">
     <?php do_action('SAH/sidebar_inner'); ?>
   </div>
